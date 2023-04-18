266cfccc4cc0150bac1d6242c9695079c49476f0
arpitha shetty <shettyarpitha856@gmail.com>
2023-04-18 14:27:38 +0530

    added second and  onpressed

--- lib/utils/button.dart
+++ lib/utils/button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_application_1/Pages/second_page.dart';
 
 class MyButton extends StatelessWidget {
   const MyButton({super.key});
@@ -8,7 +9,13 @@ class MyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => SecondPage(),
+            ));
+      },
       child: Text("Press Here"),
       style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
     );
